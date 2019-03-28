7124c56b6a1850b0c49cf306a37744bb45945310
jc462814 <Rmkm7799>
2019-03-28 14:19:44 +1000

    change variable name

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,6 +1,6 @@
 import random
 
-RANGE = 6
+MAXIMUM_LIST_LENGTH = 6
 MINIMUM_NUMBER = 1
 MAXIMUM_NUMBER = 45
 
@@ -16,7 +16,7 @@ def main():
 
 def generate_quick_pick():
     numbers = []
-    while len(numbers) < RANGE:
+    while len(numbers) < MAXIMUM_LIST_LENGTH:
         random_number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
         if random_number not in numbers:
             numbers.append(random_number)
