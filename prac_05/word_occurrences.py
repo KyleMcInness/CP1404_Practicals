7706fefb073456563632f507b4a5396bee5b0eb9
jc462814 <Rmkm7799>
2019-04-04 14:30:10 +1000

    change variable names

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,7 +7,7 @@ word_to_frequency = {}
 for word in words:
     word_to_frequency[word] = word_to_frequency.get(word, 0) + 1
 
-longest_word = max(len(word) for word in words)
+longest_word_length = max(len(word) for word in words)
 
-for key in word_to_frequency:
-    print("{:{}} : {} times".format(key, longest_word, word_to_frequency[key]))
+for word in word_to_frequency:
+    print("{:{}} : {} times".format(word, longest_word_length, word_to_frequency[word]))
