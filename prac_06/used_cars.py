38cce09f377609f5cdc53be947bdf384b3ef2418
jc462814 <Rmkm7799>
2019-04-11 14:22:54 +1000

    add comment

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -7,7 +7,7 @@ from prac_06.car import Car
 def main():
     """Demo test code to show how to use car class."""
 
-    # 1
+    # 1 amd #7
     limo = Car(100, "Limo")
     # 2
     limo.add_fuel(20)
@@ -27,6 +27,15 @@ def main():
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    # my_car = Car(100, input("Name: "))
+    # my_car.add_fuel(int(input("How much fuel do you want to add?\n"
+    #                           "> ")))
+    # print("The {}'s fuel is {}L.".format(my_car.name, my_car.fuel))
+    # my_car.drive(int(input("How far would you like the car to drive?\n"
+    #                        ">")))
+    # print("The {}'s odometer is {}km.".format(my_car.name, my_car.odometer))
+    # print(my_car)
 
 
 main()
