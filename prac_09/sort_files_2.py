a6ea1dcbdfb80733c71c034163106a76bf615544
kyle <kyle.mcinness@my.jcu.edu.au>
2019-05-22 21:22:50 +1000

    Add completed task

---
+++ prac_09/sort_files_2.py
@@ -0,0 +1,26 @@
+import shutil
+import os
+
+
+def main():
+    os.chdir('FilesToSort2')
+
+    extensions = [filename.split('.') for filename in os.listdir()]
+    chosen_categories = []
+
+    key_to_extension = {}
+
+    for i, extension in enumerate(extensions):
+        key_to_extension[extension[1]] = i
+
+    for key in key_to_extension:
+        choice = input("What category would you like to sort {} files into? ".format(key))
+        chosen_categories.append(choice)
+        if not os.path.isdir(choice):
+            os.mkdir("{}".format(choice))
+        for extension in extensions:
+            if "{}".format(key) == extension[1]:
+                shutil.move(extension[0] + '.' + extension[1], choice)
+
+
+main()
